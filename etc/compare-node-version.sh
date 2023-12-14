67a935fac25a9f16c1586ed7ee0a15a44631858a
Nicholas Wong <nickwph@gmail.com>
2023-12-14 10:55:21 +0800

    Added more tests.

--- etc/compare-node-version.sh
+++ etc/compare-node-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 nodever="$(node --version)"
 currentver="${nodever:1}"
-requiredver="21.0.0"
+requiredver="18.0.0"
 if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
   echo "Greater than or equal to ${requiredver}"
   exit 0

a986878021ab33c8069498a343229608f4665f8e
Nicholas Wong <nickwph@gmail.com>
2023-12-14 10:57:15 +0800

    Updated loggings.

--- etc/compare-node-version.sh
+++ etc/compare-node-version.sh
@@ -3,9 +3,8 @@ nodever="$(node --version)"
 currentver="${nodever:1}"
 requiredver="18.0.0"
 if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
-  echo "Greater than or equal to ${requiredver}"
   exit 0
 else
-  echo "Less than ${requiredver}"
+  echo "Current NodeJS version is v${currentver}, which is lower than required version v${requiredver}"
   exit 1
 fi
